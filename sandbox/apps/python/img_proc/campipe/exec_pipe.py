04a62ec90672b162cf19b98b8ab272940f739f4e
Uday Bondhugula <udayb@iisc.ac.in>
2017-12-18 05:11:21 -0500

    cam pipe: lose unnecessary timing output
    Signed-off-by: Uday Bondhugula <udayb@iisc.ac.in>

--- sandbox/apps/python/img_proc/campipe/exec_pipe.py
+++ sandbox/apps/python/img_proc/campipe/exec_pipe.py
@@ -63,8 +63,8 @@ def campipe(app_data):
 
         time_taken = float(t2) - float(t1)
         avg += time_taken
-        print("")
-        print("[exec_pipe] : time taken to execute = ", (time_taken * 1000), " ms")
+        #print("")
+        #print("[exec_pipe] : time taken to execute = ", (time_taken * 1000), " ms")
 
         it += 1
 
